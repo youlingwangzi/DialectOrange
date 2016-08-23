4dffe6b193917286c3e2cd6c0027bce8417d8438
yuanxiaokun <yuanxiaokun@outlook.com>
2016-08-23 22:56:43 +0800

    完成首页显示，轮播图片。

---
+++ src/main/java/com/fycz/Fragment/HomePage.java
@@ -0,0 +1,241 @@
+package com.fycz.fragment;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.ImageView;
+
+import com.fycz.dialectorange.MainActivity;
+import com.fycz.dialectorange.R;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomePage#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomePage extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private ImageHandler handler = new ImageHandler(new WeakReference<HomePage>(this));
+    private ViewPager viewPager;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public HomePage() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomePage.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomePage newInstance(String param1, String param2) {
+        HomePage fragment = new HomePage();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        //初始化iewPager的内容
+        viewPager = (ViewPager) view.findViewById(R.id.home_page_vp);
+        ImageView view1 = (ImageView) inflater.inflate(R.layout.item, null);
+        ImageView view2 = (ImageView) inflater.inflate(R.layout.item, null);
+        ImageView view3 = (ImageView) inflater.inflate(R.layout.item, null);
+        view1.setImageResource(R.drawable.title_pic1);
+        view2.setImageResource(R.drawable.title_pic2);
+        view3.setImageResource(R.drawable.title_pic3);
+        ArrayList<ImageView> views = new ArrayList<>();
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+        viewPager.setAdapter(new ImageAdapter(views));
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            //配合Adapter的currentItem字段进行设置。
+            @Override
+            public void onPageSelected(int arg0) {
+                handler.sendMessage(Message.obtain(handler, ImageHandler.MSG_PAGE_CHANGED, arg0, 0));
+            }
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+            //覆写该方法实现轮播效果的暂停和恢复
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                switch (arg0) {
+                    case ViewPager.SCROLL_STATE_DRAGGING:
+                        handler.sendEmptyMessage(ImageHandler.MSG_KEEP_SILENT);
+                        break;
+                    case ViewPager.SCROLL_STATE_IDLE:
+                        handler.sendEmptyMessageDelayed(ImageHandler.MSG_UPDATE_IMAGE, ImageHandler.MSG_DELAY);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        viewPager.setCurrentItem(Integer.MAX_VALUE/2);//默认在中间，使用户看不到边界
+        //开始轮播效果
+        handler.sendEmptyMessageDelayed(ImageHandler.MSG_UPDATE_IMAGE, ImageHandler.MSG_DELAY);
+
+        return view;
+    }
+
+
+
+    private static class ImageHandler extends Handler {
+        /**
+         * 请求更新显示的View。
+         */
+        protected static final int MSG_UPDATE_IMAGE  = 1;
+        /**
+         * 请求暂停轮播。
+         */
+        protected static final int MSG_KEEP_SILENT   = 2;
+        /**
+         * 请求恢复轮播。
+         */
+        protected static final int MSG_BREAK_SILENT  = 3;
+        /**
+         * 记录最新的页号，当用户手动滑动时需要记录新页号，否则会使轮播的页面出错。
+         * 例如当前如果在第一页，本来准备播放的是第二页，而这时候用户滑动到了末页，
+         * 则应该播放的是第一页，如果继续按照原来的第二页播放，则逻辑上有问题。
+         */
+        protected static final int MSG_PAGE_CHANGED  = 4;
+
+        //轮播间隔时间
+        protected static final long MSG_DELAY = 3000;
+
+        //使用弱引用避免Handler泄露.这里的泛型参数可以不是Activity，也可以是Fragment等
+        private WeakReference<HomePage> weakReference;
+
+        private int currentItem = 0;
+
+        protected ImageHandler(WeakReference<HomePage> wk){
+            weakReference = wk;
+        }
+
+        @Override
+
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            HomePage activity = weakReference.get();
+            if (activity==null){
+                //Activity已经回收，无需再处理UI了
+                return ;
+            }
+            //检查消息队列并移除未发送的消息，这主要是避免在复杂环境下消息出现重复等问题。
+            if (activity.handler.hasMessages(MSG_UPDATE_IMAGE)){
+                activity.handler.removeMessages(MSG_UPDATE_IMAGE);
+            }
+
+            switch (msg.what) {
+                case MSG_UPDATE_IMAGE:
+                    currentItem++;
+                    activity.viewPager.setCurrentItem(currentItem);
+                    //准备下次播放
+                    activity.handler.sendEmptyMessageDelayed(MSG_UPDATE_IMAGE, MSG_DELAY);
+                    break;
+                case MSG_KEEP_SILENT:
+                    //只要不发送消息就暂停了
+                    break;
+                case MSG_BREAK_SILENT:
+                    activity.handler.sendEmptyMessageDelayed(MSG_UPDATE_IMAGE, MSG_DELAY);
+                    break;
+                case MSG_PAGE_CHANGED:
+                    //记录当前的页号，避免播放的时候页面显示不正确。
+                    currentItem = msg.arg1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+
+
+    private class ImageAdapter extends PagerAdapter {
+
+        private ArrayList<ImageView> viewlist;
+        public ImageAdapter(ArrayList<ImageView> viewlist) {
+            this.viewlist = viewlist;
+        }
+
+        @Override
+        public int getCount() {
+            //设置成最大，使用户看不到边界
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0==arg1;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position,
+                                Object object) {
+            //Warning：不要在这里调用removeView
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            //对ViewPager页号求模取出View列表中要显示的项
+            position %= viewlist.size();
+            if (position<0){
+                position = viewlist.size()+position;
+            }
+            ImageView view = viewlist.get(position);
+            //如果View已经在之前添加到了一个父组件，则必须先remove，否则会抛出IllegalStateException。
+            ViewParent vp =view.getParent();
+            if (vp!=null){
+                ViewGroup parent = (ViewGroup)vp;
+                parent.removeView(view);
+            }
+            container.addView(view);
+            //add listeners here if necessary
+            return view;
+        }
+    }
+}
+
+
--- src/main/java/com/fycz/dialectorange/MainActivity.java
+++ src/main/java/com/fycz/dialectorange/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import com.flyco.tablayout.CommonTabLayout;
 import com.flyco.tablayout.listener.CustomTabEntity;
 
+import com.fycz.fragment.HomePage;
 import com.fycz.entity.*;
 
 import java.util.ArrayList;
@@ -17,12 +18,14 @@ public class MainActivity extends FragmentActivity {
     private Context mContext = this;
     private ArrayList<Fragment> mFragments = new ArrayList<>();
 
-    private String[] mTitles = {"首页", "寻味", "果子" , "后花园", "我的"};
+    private String[] mTitles = {"首页", "寻味", "" , "后花园", "我的"};
     private int[] mIconUnselectIds = {
-            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,R.mipmap.orange_nocolor,
+            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,
+            R.mipmap.orange_nocolor,
             R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
     private int[] mIconSelectIds = {
-            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,R.mipmap.orange,
+            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,
+            R.mipmap.orange,
             R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
     private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
     private CommonTabLayout mTabLayout_1;
@@ -34,6 +37,12 @@ public class MainActivity extends FragmentActivity {
         for (int i = 0; i < mTitles.length; i++) {
             mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
         }
+
+        mFragments.add(HomePage.newInstance("",""));
+        mFragments.add(HomePage.newInstance("",""));
+        mFragments.add(HomePage.newInstance("",""));
+        mFragments.add(HomePage.newInstance("",""));
+        mFragments.add(HomePage.newInstance("",""));
 
         mTabLayout_1 = (CommonTabLayout) findViewById(R.id.tl_1);
         mTabLayout_1.setTabData(mTabEntities, this,R.id.fl_change, mFragments);
