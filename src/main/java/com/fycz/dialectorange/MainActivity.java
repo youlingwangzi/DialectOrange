8eb87cfd7c361a4cd2cd2ff0f90a17e7876a4531
yuanxiaokun <yuanxiaokun@outlook.com>
2016-08-23 16:28:41 +0800

    完成启动页和主页框架

--- src/main/java/com/fycz/dialectorange/MainActivity.java
+++ src/main/java/com/fycz/dialectorange/MainActivity.java
@@ -1,14 +1,43 @@
 package com.fycz.dialectorange;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+
+import com.fycz.entity.*;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends FragmentActivity {
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+
+    private String[] mTitles = {"首页", "寻味", "果子" , "后花园", "我的"};
+    private int[] mIconUnselectIds = {
+            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,R.mipmap.orange_nocolor,
+            R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,R.mipmap.orange,
+            R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private CommonTabLayout mTabLayout_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+
+        mTabLayout_1 = (CommonTabLayout) findViewById(R.id.tl_1);
+        mTabLayout_1.setTabData(mTabEntities, this,R.id.fl_change, mFragments);
+
+
     }
 }
--- src/main/java/com/fycz/dialectorange/StartActivity.java
+++ src/main/java/com/fycz/dialectorange/StartActivity.java
@@ -1,8 +1,10 @@
 package com.fycz.dialectorange;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends Activity {
 
@@ -10,5 +12,11 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void onClickWelcomeButton(View view){
+        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
